cef5c69813a85700f8f4d502c15f75042a3f492b
Vivekanand P V <vivekanandpv@outlook.com>
2022-12-15 12:47:40 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task.Study
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
